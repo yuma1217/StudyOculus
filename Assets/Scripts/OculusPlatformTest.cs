c8de27426fb34004807bb8832cd8f0dee5c8bd1b
matsumura-yu <matsumura.yu@isid.co.jp>
2019-01-17 16:47:42 +0900

    Add awake and initialization and empty class

---
+++ Assets/Scripts/OculusPlatformTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Platform;
+public class OculusPlatformTest : MonoBehaviour {
+
+	protected OculusPlatformTest s_instance = null;
+
+	protected RoomTest roomTest;
+
+	protected VoipTest voipTest;
+	void Awake(){
+		if(s_instance != null){
+			Destroy(gameObject);
+			return;
+		}
+
+		s_instance = this;
+		DontDestroyOnLoad(gameObject);
+
+		TransitionToState(State.INITIALIZING);
+		Debug.Log("test");
+		Core.Initialize();
+
+		roomTest = new RoomTest();
+		voipTest = new VoipTest();
+
+	}
+
+	// Use this for initialization
+    void Start () {
+		
+	}
+
+    private void TransitionToState(State newState)
+    {
+
+    }
+
+	public enum State{
+		INITIALIZING,
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/RoomTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTest {
+
+	// 自分がサーバーかどうか
+	private bool amIserver;
+
+	// スタートアップが終わったかどうか
+	private bool startupDone;
+	public RoomTest(){
+		amIserver = false;
+		startupDone = false;
+		// todo : rooms.setなどのコールバック設定
+	}
+}
