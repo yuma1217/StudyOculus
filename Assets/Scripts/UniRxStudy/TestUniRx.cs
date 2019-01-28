0e5ea2900bcf34b31c03a27f8461cc52501ed908
matsumura-yu <matsumura.yu@isid.co.jp>
2019-01-28 17:55:41 +0900

    Add UniRx

---
+++ Assets/Scripts/UniRxStudy/TestUniRx.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+public class TestUniRx : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		
+		this.UpdateAsObservable()
+			.Subscribe(_ => {
+				cube.transform.Translate(Vector3.right * Time.deltaTime);
+			});
+	}
+	
+}
